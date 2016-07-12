b851b31a413880c4c4d9ce773ec03b8b40636db6
anant <anant.rao07@gmail.com>
2016-07-12 03:43:04 -0300

    regular expressions improved

--- src/com/examle/anant/regex/RegEx.java
+++ src/com/examle/anant/regex/RegEx.java
@@ -12,9 +12,6 @@ import java.util.regex.Pattern;
 public class RegEx {
 
 
-	
-	
-	
 	   public static void main(String[] args) {
 		   HashMap<String, String> customerDetails = new HashMap<String, String>();
 		   ArrayList<String> names = new ArrayList<String>();
@@ -22,27 +19,22 @@ public class RegEx {
 		   
 	        Scanner in = new Scanner(System.in);
 	        int N = in.nextInt();
-	       
-	        //String[][] customerDetails = new String[N][N];
+	  
 	        for(int a0 = 0; a0 < N; a0++){
 	            String firstName = in.next();
 	            String emailID = in.next();
 	            customerDetails.put(emailID, firstName);
 	            
 	        }
-	        String myregexString = "[a-z]+"+"\\p{Punct}+"+"gmail.com";
-	        
-	   
-	  
-	        int i = 0;
+	        in.close();
+	        //String myregexString = "[a-z]+"+"\\p{Punct}+"+"gmail.com";
+	        String myregexString = ".+\\@gmail.com";
+	    
 	        for(String element:customerDetails.keySet()){
 	        
-	        	//System.out.println(element);
-	        	//System.out.println(customerDetails.get(element));
-	        	//System.out.println(element.matches(myregexString));
+	  
 	        	 if(element.matches(myregexString) ){
-	        		//cNames[i] = customerDetails.get(element);
-	        		//i++;
+	        		
 	        		names.add(customerDetails.get(element)) ;
 	        	 }
 	         
@@ -52,12 +44,7 @@ public class RegEx {
 	        for(String s : names){
 	        	System.out.println(s);
 	        }
-	       // String cNames[] = new String[i];
-	      
-	        
-	       // Arrays.sort(cNames);
-	       // for(int j = 0 ;j<cNames.length ; j++){
-	       // System.out.println(cNames[j]);
+	     
 	        	
 	        }
 	        
